faad9acc7ba6feda559efc5e64325600b70d8a2b
Prakhar Dubey <prakhardubey2002@gmail.com>
2023-09-08 02:16:27 +0530

    Home page completed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var swiper = new Swiper('.swiper-container', {
     loop:true,
     autoplay:{
-        delay:2500,
+        delay:2000,
     },
     effect: 'fade',
     slidesPerView: 1, // Number of slides per view
@@ -10,4 +10,14 @@ var swiper = new Swiper('.swiper-container', {
       nextEl: '.swiper-button-next', // Next button class
       prevEl: '.swiper-button-prev', // Previous button class
     },
+  });
+var swiper = new Swiper('.swiper-container2', {
+    loop:true,
+    autoplay:{
+        delay:4000,
+    },
+    effect: 'slide',
+    slidesPerView: 1, // Number of slides per view
+    spaceBetween: 1, // Space between slides (optional)
+    
   });
